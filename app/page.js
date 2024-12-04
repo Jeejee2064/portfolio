65a26d834df8a95888b33c027115be18d056ca20
Jeejee2064 <j.de.watteville@gmail.com>
2024-12-04 17:36:18 -0500

    first commit

--- app/page.js
+++ app/page.js
@@ -3,17 +3,12 @@ import Hero from './components/Hero'
 import ScrollShadow from './components/ScrollShadow'
 import WorkSection from './components/WorkSection'
 import AboutMe from './components/AboutMe'
+import Contact from './components/Contact'
 
 export default function Main() {
   return (
     <main className="relative">
       <Hero />
-      <section >
-        <WorkSection />
-      </section>
-      <section >
-        <AboutMe />
-      </section>
 
     </main>
   );
